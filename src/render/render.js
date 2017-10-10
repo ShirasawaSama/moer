0211ba5a6ca680773f564e005bbd7760aac7d7c4
ncbql <764798966@qq.com>
2017-10-10 13:39:54 +0800

    调整一下render的循环逻辑

--- src/render/render.js
+++ src/render/render.js
@@ -9,13 +9,15 @@ export default document => function render (node, parent) {
       case 'object':
         if (Array.isArray(node)) {
           const keys = {}
-          node.forEach((node, i) => {
-            const dom = render(node, parent)
+          let i = 0
+          for (const elm of node) {
+            const dom = render(elm, parent)
             if (dom) {
               parent.appendChild(dom)
-              if (node.a && typeof node.a.key !== 'undefined') keys[node.a.key] = i
+              if (elm.a && typeof elm.a.key !== 'undefined') keys[elm.a.key] = i
             }
-          })
+            i++
+          }
           node.k = keys
         }
         let { t: type = 'div', c: children, a: args } = node
@@ -25,7 +27,7 @@ export default document => function render (node, parent) {
         const elm = isSvg
           ? document.createElementNS('http://www.w3.org/2000/svg', 'svg')
           : document.createElement(type)
-        if (args) Object.entries(args).forEach(([key, value]) => key !== 'key' && setAccessor(elm, key, null, value, isSvg))
+        if (args) for (const key in args) if (key !== 'key') setAccessor(elm, key, null, args[key], isSvg)
         if (Array.isArray(children)) children.forEach(node => (node = render(node, elm)) && elm.appendChild(node))
         return elm
     }
